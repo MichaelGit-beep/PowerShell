3166fd6dabb72f307d618a2559f3d886940c21aa
MichaelGit-beep <61975960+MichaelGit-beep@users.noreply.github.com>
2021-04-05 13:13:01 +0300

    Update MountNEtDrive.ps1

--- MountNEtDrive.ps1
+++ MountNEtDrive.ps1
@@ -1,1 +1,3 @@
 New-SmbMapping -LocalPath E: -RemotePath \\192.168.77.204\install -Persistent:$true -UserName root -Password Magal1234! -SaveCredentials
+#Get-SmbMapping 
+#Remove-SmbMapping -RemotePath "\\SO\VMFiles" -Force
